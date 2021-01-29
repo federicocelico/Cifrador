0961cf85e4ecc0b31cb5f8b974b076de95b71800
federicocelico <federico.celico@gmail.com>
2021-01-29 10:53:08 -0300

    Alter Class Clave

---
+++ Cifrador/Clave.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cifrador;
+
+namespace ConsoleApp1
+{
+   public class Clave
+    {
+        public static string MailPassword;
+        public static string clave = "INGRESAR CADENA A PROCESAR";
+        public static string LoadValues()
+        {
+            AESObjectEnc Encriptacion = new AESObjectEnc();
+
+            MailPassword = Encriptacion.Decrypt(clave); //CAMBIAR A ENCRYPYT O DESCRYPT SEGUN CORRESPONDA
+            return MailPassword;
+        }
+
+
+
+    }
+}
+
