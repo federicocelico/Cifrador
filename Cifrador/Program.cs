bf9941d76c156d5fe2273fe889590b42c5cd2cf0
federicocelico <federico.celico@gmail.com>
2021-01-29 10:46:13 -0300

    First Commit

---
+++ Cifrador/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    { public static string clave;
+        static void Main(string[] args)
+        {
+            clave = Clave.LoadValues();
+            Console.WriteLine(clave);
+            Console.ReadKey();
+        }
+    }
+}
